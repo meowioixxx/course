47473cbf68aa6e86df1a92080005a0e52b44db18
Paranoialove <akazuma82@gmail.com>
2016-01-12 15:51:34 +0800

    HelloJava

---
+++ U220/mod07/HelloJava.java
@@ -0,0 +1,8 @@
+public class HelloJava{
+	//一個公開java類別名為HelloJava
+	//起始程式main
+	public static void main(String[] args){
+		System.out.println("Hello Java!");
+		//System.out.println 印出之後帶一換行字符//		
+	}			
+}
