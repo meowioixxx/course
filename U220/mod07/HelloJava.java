20d2dd22225bd6ddcc8658ac51d428c33a2acb3f
Paranoialove <akazuma82@gmail.com>
2016-01-12 16:42:29 +0800

    修改註解

--- U220/mod07/HelloJava.java
+++ U220/mod07/HelloJava.java
@@ -1,6 +1,6 @@
 public class HelloJava{
 	//一個公開java類別名為HelloJava
-	//起始程式main
+	//起始程式main  公開靜態不回傳  裡面只有一個字串陣列叫args
 	public static void main(String[] args){
 		System.out.println("Hello Java!");
 		//System.out.println 印出字串之後帶一換行字符//		
