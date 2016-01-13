e2e5ded6c1e74bc9c3158a8fb22d3271c8740b05
Paranoialove <akazuma82@gmail.com>
2016-01-13 11:27:00 +0800

    netbeans JAVA start

---
+++ U220/mod08/src/HelloJava.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+    //快速鍵 psvm  TAB
+        System.out.println("Hello，5509同學們");
+        //尋找建議字：ex.  sys  >>>   ctrl ﹨   >>>  System        
+    }    
+}
