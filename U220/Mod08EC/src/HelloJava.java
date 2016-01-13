cb281d390b2723c2dea1a5b48c79e1a74fa12a1a
Paranoialove <akazuma82@gmail.com>
2016-01-13 14:07:57 +0800

    0

--- U220/Mod08EC/src/HelloJava.java
+++ U220/Mod08EC/src/HelloJava.java
@@ -5,7 +5,8 @@ public class HelloJava {
 		// TODO Auto-generated method stub
 		System.out.println("hihihihhi我是eclipse阿喆");
 		//快速鍵sysout   alt /
-		
+		//命名慣例：大寫>>類別    小寫>>屬性
+		//System 預設類別 在JRE System Library(rt.jar)
 	}
 
 }
