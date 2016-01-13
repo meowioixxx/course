aeb4f9c9b3e733d9f82691d18d8c65c8038a9ac1
Paranoialove <akazuma82@gmail.com>
2016-01-13 16:57:19 +0800

    區域變數練習

---
+++ U220/mod08/src/TestLocalVariables.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author Administrator
+ */
+public class TestLocalVariables {
+
+    public static void main(String[] args) {
+        int i; //宣告區域變數i 只在此方法內宣告後能使用
+        i = 1;//指派i 為1
+        System.out.println("i= " + i);
+        int j = 100;
+        System.out.println("j= " + j);
+        //快速鍵：soutv [TAB]
+    }
+}
