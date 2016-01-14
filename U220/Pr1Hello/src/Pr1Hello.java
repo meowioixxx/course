95ef03b3c39b61ca4c54d84d2c9b97a3ef542faf
Paranoialove <akazuma82@gmail.com>
2016-01-14 15:11:20 +0800

    練習：整數與浮點數

---
+++ U220/Pr1Hello/src/Pr1Hello.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author 我是綿綿
+ * 今天是第一次自習課
+ * 練習整數與浮點數
+ */
+public class Pr1Hello {
+
+    public static void main(String[] args) {
+        byte a = -11;
+        short b = -254;
+        int i = 1;
+        long j = 351687116;
+        float k = 1.0015165999991F;
+        double m = 1.0015165999991;
+        System.out.println("<<基本資料型別練習>>");
+        System.out.println("整數類：");
+        System.out.println("byte a = -11 >>" + a);
+        System.out.println("short b = -254 >>" + b);
+        System.out.println("int i = 1 >>" + i);
+        System.out.println("long j = 351687116 >>" + j);
+        System.out.println("浮點數類：");
+        System.out.println("float k = 1.0015165999991F >>" + k);
+        System.out.println("double m = 1.0015165999991 >>" + m);
+        System.out.println("Hello 給我練習!!!");
+    }
+
+}
