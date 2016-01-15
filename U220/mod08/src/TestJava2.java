56d69593f4a345a36fb23e4380e676dc77a38072
Paranoialove <akazuma82@gmail.com>
2016-01-15 15:14:08 +0800

    native2ascii編碼修改練習

---
+++ U220/mod08/src/TestJava2.java
@@ -0,0 +1,6 @@
+public class TestJava2{
+    public static void main(String[] args){
+        System.out.println("\u7df4\u7fd2\u76f4\u63a5\u6539\u526f\u6a94\u540d\uff1f");
+		//\u8a66\u8a66\u770b~~~
+    }
+}
